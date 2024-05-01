1c801b7edd75008fecbe509f109719004f121552
chaosdev507 <97633663+chaosdev507@users.noreply.github.com>
2024-05-01 07:49:19 -0500

    Update sign_in_screen.dart removing blank line

--- lib/features/sign_in/presentations/view/sign_in_screen.dart
+++ lib/features/sign_in/presentations/view/sign_in_screen.dart
@@ -33,7 +33,6 @@ class _SignInScreenState extends ConsumerState<SignInScreen> {
 
   Future<void> _signIn() async {
     if (!formKey.currentState!.validate()) return;
-
     ref
         .read(signInControllerProvider.notifier)
         .signIn(email: Email(email), password: Password(password));
